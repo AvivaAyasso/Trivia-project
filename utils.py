63e8ba49c7e14229a0757be754c6ec60a024026e
betty melaku <melakubetty@gmail.com>
2022-04-03 18:39:45 +0300

    utils

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 #------------------------
-from part1 import options,questions
+# from part1 import options,questions
 def new_game():
 
     guesses = []
@@ -57,4 +57,23 @@ def play_again():
         return True
     else:
         return False
+
+questions = {
+    "What is the correct file extention for python files?": "B",
+    "Which operators is used to multiply?": "B",
+    "Which collection is ordered, changeable, and allows duplicate numbers?": "A",
+}
+
+options = [
+    ["A. '.pyt'", "B. '.py'", "C. '.pt'", "D. '.pyth'"],
+    ["A. #", "B. *", "C. +", "D. x"],
+    ["A. List", "B. Dictionary", "C. Tuple", "D. Set"]
+]
+
+new_game()
+
+while play_again():
+    new_game()
+
+print("Byeeeeee!")
 
