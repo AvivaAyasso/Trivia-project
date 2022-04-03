5bab325d0d801f357d99dca9a31dbea04fe97032
betty melaku <melakubetty@gmail.com>
2022-04-03 18:16:58 +0300

    utils.

--- utils.py
+++ utils.py
@@ -1,6 +1,24 @@
 #------------------------
+
 def new_game():
-    pass
+    def new_game():
+
+        guesses = []
+        correct_guesses = 0
+        question_num = 1
+
+        for key in questions:
+            print("-------------------------")
+            print(key)
+            for i in options[question_num - 1]:
+                print(i)
+            guess = input("Enter (A, B, C, or D): ")
+            guess = guess.upper()
+            guesses.append(guess)
+
+            correct_guesses += check_answer(questions.get(key), guess)
+
+
 #------------------------
 def check_answer():
     pass
