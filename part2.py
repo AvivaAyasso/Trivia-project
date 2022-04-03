18dcb10d524ce10db2b96ac5e1881302c19f4715
aviva <avivaayasso@icloud.com>
2022-04-03 18:07:47 +0300

    part 2.

--- part2.py
+++ part2.py
@@ -1,3 +1,12 @@
-aviva =1
-avi = 2
-print(aviva + avi)
+
+questions = {
+ "Who created Python?: ": "A",
+ "What year was Python created?: ": "B",
+ "Python is tributed to which comedy group?: ": "C",
+
+}
+
+options = [["A. Guido van Rossum", "B. Aviva Ayasso", "C. Bill Gates", "D. Mark Zuckerburg"],
+          ["A. 1989", "B. 1991", "C. 2000", "D. 2022"],
+          ["A. Lonely Island", "B. Smosh", "C. Monty Python", "D. SNL"],
+          ]
