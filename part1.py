722caa9bc753344d0d1d768d5e531c520c862b9d
betty melaku <melakubetty@gmail.com>
2022-04-03 18:03:29 +0300

    part1.

--- part1.py
+++ part1.py
@@ -1,3 +1,11 @@
 qestions = {
-    ""
+    "What is the correct file extention for python files?": "B",
+    "Which operators is used to multiply?": "B",
+    "Which collection is ordered, changeable, and allows duplicate numbers?": "A",
 }
+
+options = [
+    ["A. '.pyt'", "B. '.py'", "C. '.pt'", "D. '.pyth'"],
+    ["A. #", "B. *", "C. +", "D. x"],
+    ["A. List", "B. Dictionary", "C. Tuple", "D. Set"]
+]
