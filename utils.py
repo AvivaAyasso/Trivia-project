f9e3b88a1fd582c339f062f40f9121ea93337e74
betty melaku <melakubetty@gmail.com>
2022-04-04 13:00:58 +0300

    utilsss

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 #------------------------
-
+from part1 import *
 
 def new_game():
 
@@ -21,7 +21,7 @@ def new_game():
         if question_num == 10 and check_answer(questions1.get(key), guess) == 1:
             correct_guesses += 1
         if question_num % 3 == 0:
-            print("your score is :", round(correct_guesses* (100/10)))
+            print("your score is :", round(correct_guesses * (100/10)))
         question_num += 1
 
     display_score(correct_guesses)
@@ -61,40 +61,8 @@ def play_again():
         return False
 
 
-questions1 = {"Who created Python?: ": "A",
-              "What year was Python created?: ": "B",
-              "Python is tributed to which comedy group?: ": "C",
-              "What is the correct file extention for python files?": "B",
-              "Which operators is used to multiply?": "B",
-              "Which collection is ordered, changeable, and allows duplicate numbers?": "A",
-              "Which statment is used to stop a loop? ": "A",
-              "What is 'init' mean? ": "D",
-              "What year Betty was barn": "B",
-              "What year Aviva was barn": "C"
-
-}
-
-
-options1 = [
-    ["A. Guido van Rossum", "B. Aviva Ayasso", "C. Bill Gates", "D. Mark Zuckerburg"],
-    ["A. 1989", "B. 1991", "C. 2000", "D. 2022"],
-    ["A. Lonely Island", "B. Smosh", "C. Monty Python", "D. SNL"],
-    ["A. '.pyt'", "B. '.py'", "C. '.pt'", "D. '.pyth'"],
-    ["A. #", "B. *", "C. +", "D. x"],
-    ["A. List", "B. Dictionary", "C. Tuple", "D. Set"],
-    ["A. break", "B. exit", "C. stop", "D. return"],
-    ["A. Meaningless name", "B. All answers are correct", "C. Object", "D. constructor"],
-    ["A. 1989", "B. 1999", "C. 2000", "D. 2002"],
-    ["A. 1989", "B. 2000", "C. 1991", "D. 2005"]
-
-    ]
-
 
 new_game()
 
-while play_again():
-    new_game()
 
-
-print("Byeeeeee!")
 
